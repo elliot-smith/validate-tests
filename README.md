49791a38b2fc29e34563a908035562155352400f
Elliot <elliot.smith@gmail.com>
2018-10-01 17:18:09 +1000

    Allows the user to pass in arguments via the command line

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # validate-tests
 A tool that checks whether you code impact your test output
+
+
+
+## Commands
+validate-tests.exe "INSERT_TEST_COMMAND" "INSERT_TEST_DIRECTORY_HERE"
+go build && validate-tests.exe "npm run test" "F:/Developer/workspace/example-typescript-nyc-mocha-coverage"
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import "fmt"
+import "strings"
 import "os/exec"
 import "os"
 
 func main() {
-    _, err := run_tests()
+    testCommand := os.Args[1]
+    directory := os.Args[2]
+
+    _, err := runTests(testCommand, directory)
 
     if ( err != nil) {
         fmt.Println("Tests did not pass without deleted lines")
@@ -15,9 +19,11 @@ func main() {
     fmt.Println("Tests passed")
 }
 
-func run_tests() (string, error) {
-    cmd := exec.Command("npm", "test");
-    cmd.Dir = "F:/Developer/workspace/example-typescript-nyc-mocha-coverage"
+func runTests(testCommand string, directory string) (string, error) {
+    splitTestCommand := strings.Split(testCommand, " ")
+    cmd := exec.Command(splitTestCommand[0])
+    cmd.Args = splitTestCommand
+    cmd.Dir = directory
     out, error := cmd.Output()
     return string(out), error
 }
