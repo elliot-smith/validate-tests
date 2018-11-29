394a0dd8598e0e56a1954ae38f95f94d53541817
Elliot <elliot.smith@gmail.com>
2018-11-29 21:00:59 +1100

    Adds a file that can be tested against as a parameter

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A tool that checks whether you code impact your test output
 
 
 ## Commands
-validate-tests.exe "INSERT_TEST_COMMAND" "INSERT_TEST_DIRECTORY_HERE"
-go build && validate-tests.exe "npm run test" "F:/Developer/workspace/example-typescript-nyc-mocha-coverage"
+validate-tests.exe "INSERT_TEST_COMMAND" "INSERT_TEST_DIRECTORY_HERE" "INSERT_TEST_FILE_HERE"
+go build && validate-tests.exe "npm run test" "F:/Developer/workspace/example-typescript-nyc-mocha-coverage" "calculator\ts\src\add.ts"
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
-import "strings"
-import "os/exec"
-import "os"
+import (
+    "fmt"
+    "strings"
+    "os/exec"
+    "os"
+    "io/ioutil"
+)
 
 func main() {
     testCommand := os.Args[1]
     directory := os.Args[2]
+    testFile := os.Args[3]
 
     // Validate that the tests can run successfully
     _, err := runTests(testCommand, directory)
@@ -16,7 +20,12 @@ func main() {
         os.Exit(1)
     }
 
+    dat, err := ioutil.ReadFile(directory + "/" + testFile)
+
+    fmt.Println(string(dat))
+
     fmt.Println("Tests passed")
+    fmt.Println(directory + testFile)
 }
 
 func runTests(testCommand string, directory string) (string, error) {
